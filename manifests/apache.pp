9f4e9956226487e85c03735bc52c57b0e264c6d9
Yoann Laissus <yoann.laissus@gmail.com>
2013-08-09 11:26:58 +0200

    Add a htpasswd specific apache config file

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -3,27 +3,6 @@ class awstats::apache (
 ) {
     include awstats
 
-    file { "/etc/awstats/apache.conf":
-        ensure  => present,
-        source => 
-            [ "puppet:///modules/site-awstats/${::fqdn}/apache.conf",
-            "puppet:///modules/site-awstats/${::operatingsystem}/apache.conf",
-            "puppet:///modules/site-awstats/apache.conf",
-            "puppet:///modules/awstats/${::operatingsystem}/apache.conf",
-            "puppet:///modules/awstats/apache.conf" ],
-        owner   => root,
-        group   => root,
-        mode    => 0644,
-        notify  => Exec["refresh_awstats"],
-        require => Package["awstats"];
-    }
-
-    apache::config::global { "awstats.conf":
-        ensure => link,
-        target => "/etc/awstats/apache.conf",
-        require => File["/etc/awstats/apache.conf"]
-    }
-
     if $use_htpasswd {
         file { 'awstats_htpasswd':
             path => "/etc/awstats/htpasswd.users",
@@ -36,5 +15,30 @@ class awstats::apache (
             require => Package['awstats'],
             mode => 0640, owner => root, group => www-data;
         }
+        $conf_file = apache_htpasswd.conf
+    }
+    else {
+        $conf_file = apache.conf
+    }
+
+    file { "/etc/awstats/apache.conf":
+        ensure  => present,
+        source => 
+            [ "puppet:///modules/site-awstats/${::fqdn}/apache.conf",
+            "puppet:///modules/site-awstats/${::operatingsystem}/apache.conf",
+            "puppet:///modules/site-awstats/apache.conf",
+            "puppet:///modules/awstats/${::operatingsystem}/${conf_file}",
+            "puppet:///modules/awstats/${conf_file}" ],
+        owner   => root,
+        group   => root,
+        mode    => 0644,
+        notify  => [Exec["refresh_awstats"], Service['apache']]
+        require => Package["awstats"];
+    }
+
+    apache::config::global { "awstats.conf":
+        ensure => link,
+        target => "/etc/awstats/apache.conf",
+        require => File["/etc/awstats/apache.conf"]
     }
 }
