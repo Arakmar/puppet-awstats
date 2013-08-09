a65fbf34e996cae9713072609ac4959f2fddf40d
Yoann Laissus <yoann.laissus@gmail.com>
2013-08-09 10:52:34 +0200

    Apache config file and htpasswd support

---
+++ manifests/apache.pp
@@ -0,0 +1,40 @@
+class awstats::apache (
+    $use_htpasswd = 'false'
+) {
+    include awstats
+
+    file { "/etc/awstats/apache.conf":
+        ensure  => present,
+        source => 
+            [ "puppet:///modules/site-awstats/${::fqdn}/apache.conf",
+            "puppet:///modules/site-awstats/${::operatingsystem}/apache.conf",
+            "puppet:///modules/site-awstats/apache.conf",
+            "puppet:///modules/awstats/${::operatingsystem}/apache.conf",
+            "puppet:///modules/awstats/apache.conf" ],
+        owner   => root,
+        group   => root,
+        mode    => 0644,
+        notify  => Exec["refresh_awstats"],
+        require => Package["awstats"];
+    }
+
+    apache::config::global { "awstats.conf":
+        ensure => link,
+        target => "/etc/awstats/apache.conf",
+        require => File["/etc/awstats/apache.conf"]
+    }
+
+    if $use_htpasswd {
+        file { 'awstats_htpasswd':
+            path => "/etc/awstats/htpasswd.users",
+            source =>
+                [ "puppet:///modules/site-awstats/configs/${::fqdn}/htpasswd.users",
+                "puppet:///modules/site-awstats/configs/${::operatingsystem}/htpasswd.users",
+                "puppet:///modules/site-awstats/configs/htpasswd.users",
+                "puppet:///modules/awstats/configs/${::operatingsystem}/htpasswd.users",
+                "puppet:///modules/awstats/configs/htpasswd.users" ],
+            require => Package['awstats'],
+            mode => 0640, owner => root, group => www-data;
+        }
+    }
+}
